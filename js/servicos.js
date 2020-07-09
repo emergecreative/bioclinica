fb580d961010e7e97054f9501ca07e397494c49a
emergecreative <contato@emergecreative.com.br>
2020-07-09 13:45:15 -0300

    Finalização página serviços

--- js/servicos.js
+++ js/servicos.js
@@ -2,7 +2,9 @@
 
 // título do submenu
 
+let servico = 
 
 window.addEventListener('load', function (){
     document.getElementById('text-title').innerText=document.title.split(" | ")[0];
+    document.getElementById('contato-automatico').innerText = "Alguma dúvida sobre " + document.title.split(" | ")[0] + "? Entre em contato com a gente que te respondemos assim que possível."
 });
